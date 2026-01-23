6d0a09da2367297b4f2f6b12abc91475893643e7
aychar <aychar@aychar.dev>
2026-01-23 15:08:11 -0600

    val placeholder

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,12 +1,14 @@
 import { FiGitCommit } from "react-icons/fi";
 import { MdArrowOutward } from "react-icons/md";
 import { IoGitPullRequestOutline } from "react-icons/io5";
+import { SiValorant } from "react-icons/si";
 
 export default function Timeline() {
   return (
     <div className="w-132 h-165 p-6 flex flex-col border border-neutral-800 bg-neutral-950">
       <GithubTimelineItem itemType="commit" />
       <GithubTimelineItem itemType="pr" />
+      <ValorantTimelineItem />
     </div>
   );
 }
@@ -19,16 +21,22 @@ function IconWrapper({ children }: { children: React.ReactNode }) {
   );
 }
 
+function TimelineItemWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex items-center gap-3 text-neutral-200">{children}</div>
+  );
+}
+
 function GithubTimelineItem({ itemType }: { itemType: "commit" | "pr" }) {
   if (itemType === "commit")
     return (
-      <div className="flex items-center gap-3 text-neutral-200">
+      <TimelineItemWrapper>
         <IconWrapper>
           <FiGitCommit size={20} />
         </IconWrapper>
         <div>
           <p>commit name</p>
-          <p className="text-xs text-neutral-400">repo name | 12 mins ago</p>
+          <p className="text-xs text-neutral-400">repo name | 5s</p>
         </div>
 
         <div className="flex-1" />
@@ -42,12 +50,12 @@ function GithubTimelineItem({ itemType }: { itemType: "commit" | "pr" }) {
             <MdArrowOutward size={20} />
           </a>
         </div>
-      </div>
+      </TimelineItemWrapper>
     );
 
   if (itemType === "pr") {
     return (
-      <div className="flex items-center gap-3 text-neutral-200">
+      <TimelineItemWrapper>
         <IconWrapper>
           <IoGitPullRequestOutline size={20} />
         </IconWrapper>
@@ -55,7 +63,7 @@ function GithubTimelineItem({ itemType }: { itemType: "commit" | "pr" }) {
           <p>
             <span className="text-neutral-400">#56</span> pr name
           </p>
-          <p className="text-xs text-neutral-400">repo name | 12 mins ago</p>
+          <p className="text-xs text-neutral-400">repo name | 12m</p>
         </div>
 
         <div className="flex-1" />
@@ -69,7 +77,36 @@ function GithubTimelineItem({ itemType }: { itemType: "commit" | "pr" }) {
             <MdArrowOutward size={20} />
           </a>
         </div>
-      </div>
+      </TimelineItemWrapper>
     );
   }
+}
+
+function ValorantTimelineItem() {
+  return (
+    <TimelineItemWrapper>
+      <IconWrapper>
+        <SiValorant size={20} />
+      </IconWrapper>
+      <div>
+        <p>Bind</p>
+        <p className="text-xs text-neutral-400">21/12/4 | 24m</p>
+      </div>
+
+      <div className="flex flex-1 flex-col items-center">
+        <p className="text-green-500">VICTORY</p>
+        <p className="text-sm text-neutral-400">13-6</p>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <div className="flex flex-col items-end">
+          <p className="text-sm text-pink-400">DIAMOND 1</p>
+          <p className="text-sm text-green-500">+24 RR</p>
+        </div>
+        <a>
+          <MdArrowOutward size={20} />
+        </a>
+      </div>
+    </TimelineItemWrapper>
+  );
 }
